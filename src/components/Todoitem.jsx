105e118857dea3b8f529201c94d96bb3567cf24f
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-05-26 22:09:52 +0530

    condition rendering for no task added

--- src/components/Todoitem.jsx
+++ src/components/Todoitem.jsx
@@ -5,24 +5,40 @@ function Todoitem(props) {
   return (
     <>
       <div className="Todo-Wrapper">
-        {props.task.map((item, index) => {
-          return (
-            <>
-              <div className="Todo-List">
-                <p key={index}>{item}</p>
-                <MdDeleteForever
-                  className="delete-button"
-                  onClick={() => {
-                    props.DeleteItem(index);
-                  }}
-                />
-              </div>
-            </>
-          );
-        })}
+        {props.task.length === 0 ? (
+          <p>No Task yet</p>
+        ) : (
+          props.task.map((item, index) => (
+            <div className="Todo-List" key={index}>
+              <p>{item}</p>
+              <MdDeleteForever
+                className="delete-button"
+                onClick={() => {
+                  props.DeleteItem(index);
+                }}
+              />
+            </div>
+          ))
+        )}
       </div>
     </>
   );
 }
 
 export default Todoitem;
+
+// {props.task.map((item, index) => {
+//     return (
+//       <>
+//         <div className="Todo-List">
+//           <p key={index}>{item}</p>
+//           <MdDeleteForever
+//             className="delete-button"
+//             onClick={() => {
+//               props.DeleteItem(index);
+//             }}
+//           />
+//         </div>
+//       </>
+//     );
+//   })}
